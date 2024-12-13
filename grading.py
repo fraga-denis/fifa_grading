b944113383fe19c9ca616add6d6011999756869b
fraga-denis <fraga.denis@gmail.com>
2024-12-13 18:00:49 +0000

    Update grading.py

--- grading.py
+++ grading.py
@@ -287,17 +287,17 @@ def post_match_grading():
             with col1:
                 stamina = st.slider(
                     f"Stamina (from 1 to 10)", 
-                    min_value=1, max_value=10, 
-                    value=5,  # Default value
-                    step=0.5,   # Step size for finer control
+                    min_value=1.0, max_value=10.0, 
+                    value=5.0,  # Default value
+                    step=0.5,   # Step size of 0.5
                     key=f"{unique_key_prefix}_stamina"
                 )
             with col2:
                 teamwork = st.slider(
                     f"Teamwork", 
-                    min_value=1, max_value=10, 
-                    value=5,  # Default value
-                    step=0.5,   # Step size for finer control
+                    min_value=1.0, max_value=10.0, 
+                    value=5.0,  # Default value
+                    step=0.5,   # Step size of 0.5
                     key=f"{unique_key_prefix}_teamwork"
                 )
             
@@ -306,17 +306,17 @@ def post_match_grading():
             with col3:
                 attacking = st.slider(
                     f"Attacking", 
-                    min_value=1, max_value=10, 
-                    value=5,  # Default value
-                    step=0.5,   # Step size for finer control
+                    min_value=1.0, max_value=10.0, 
+                    value=5.0,  # Default value
+                    step=0.5,   # Step size of 0.5
                     key=f"{unique_key_prefix}_attacking"
                 )
             with col4:
                 defending = st.slider(
                     f"Defending", 
-                    min_value=1, max_value=10, 
-                    value=5,  # Default value
-                    step=0.5,   # Step size for finer control
+                    min_value=1.0, max_value=10.0, 
+                    value=5.0,  # Default value
+                    step=0.5,   # Step size of 0.5
                     key=f"{unique_key_prefix}_defending"
                 )
 
